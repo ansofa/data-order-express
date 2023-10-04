9f4d8fc6917fb2114640fcc2e5579d8f1963fba6
ansofa <amursyidansofa@gmail.com>
2023-10-04 21:12:39 +0700

    edit fs

--- models/data-order-model.js
+++ models/data-order-model.js
@@ -1,10 +1,8 @@
-// const path = require("path");
 const fs = require("node:fs/promises");
 
 class DataOrder {
   async read() {
     try {
-    //   const databaseDirectory = path.join(process.cwd(), "database");
       const file = await fs.readFile("/tmp/data_order.json", {
         encoding: "utf-8",
       });
@@ -16,7 +14,6 @@ class DataOrder {
 
   async save(body) {
     try {
-    //   const databaseDirectory = path.join(process.cwd(), "database");
       await fs.writeFile("/tmp/data_order.json", JSON.stringify(body, null, 2));
       return "Success";
     } catch (error) {
