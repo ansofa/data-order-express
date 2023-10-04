06cbcc7befada114ff79a5263031f128eb470574
ansofa <amursyidansofa@gmail.com>
2023-10-04 19:30:55 +0700

    first commit

--- models/data-order-model.js
+++ models/data-order-model.js
@@ -3,7 +3,7 @@ const fs = require("node:fs/promises");
 class DataOrder {
   async read() {
     try {
-      const file = await fs.readFile("./database/data_order.json", {
+      const file = await fs.readFile("../database/data_order.json", {
         encoding: "utf-8",
       });
       return eval(file);
